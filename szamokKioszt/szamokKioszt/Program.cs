bae80acff5602efa6e39ebb6a3a81e9af34bcb99
12.c Szélpál Barna <116707386+c0szebar@users.noreply.github.com>
2025-01-10 13:49:51 +0100

    szamok

---
+++ szamokKioszt/szamokKioszt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace szamokKioszt
+{
+    class Program
+    {
+        static void Main()
+        {
+            // A fájl elérési útja
+            string filePath = "similar_file.txt";
+
+            //a fájl létezik-e
+            if (File.Exists(filePath))
+            {
+                // Fájl beolvasása
+                string fileContent = File.ReadAllText(filePath);
+
+                // A fájl tartalmának megjelenítése a konzolon
+                Console.WriteLine("A fájl tartalma:");
+                Console.WriteLine(fileContent);
+            }
+            else
+            {
+                Console.WriteLine("A fájl nem található.");
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
